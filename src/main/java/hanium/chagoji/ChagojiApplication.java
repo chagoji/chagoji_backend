79da86a9b5b9879ceb9ee6f7a85de560e7abb6b1
GitHyeon <ryuoo0123@gmail.com>
2024-05-25 13:21:03 +0900

    initial commit

---
+++ src/main/java/hanium/chagoji/ChagojiApplication.java
@@ -0,0 +1,13 @@
+package hanium.chagoji;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChagojiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChagojiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hanium/chagoji/ChagojiApplicationTests.java
@@ -0,0 +1,13 @@
+package hanium.chagoji;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChagojiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
